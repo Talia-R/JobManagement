539bd7621bfddc39bfdab6b3f3daf6a45b875c3b
Talia-R <natalia.1067@hotmail.com>
2025-10-26 10:02:58 -0300

    add Post

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -3,23 +3,20 @@ package br.com.rocketseat.gestao_vagas.modules.candidate.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
+import br.com.rocketseat.gestao_vagas.modules.candidate.model.Candidate;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
-    // @PostMapping("/{name}")
-    // public String createCanditate(@PathVariable String name) {
-    //     //TODO: process POST request
-        
-    //     return entity;
-    // }
+    @PostMapping("/")
+    public void createCanditade(@RequestBody Candidate newCanditade) {
+        System.out.println("Candidate: ");
+        System.out.println(newCanditade.createDTO());
+    }
     
     public void createCandidate(){}
 
