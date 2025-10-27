55e40e27391f295f93ef4a2138ca6d2e11f49446
Talia-R <natalia.1067@hotmail.com>
2025-10-27 07:57:40 -0300

    adjusted model

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/model/Candidate.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/model/Candidate.java
@@ -2,15 +2,26 @@ package br.com.rocketseat.gestao_vagas.modules.candidate.model;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
 import br.com.rocketseat.gestao_vagas.modules.candidate.candidateDTO.CandidateDTO;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data // add getters and sets automatically by lombok
 public class Candidate {
     private UUID id;
+
     private String name;
-    private String username;
+
+    @Email(message = "O campo deve conter um e-mail válido.")
     private String email;
+
+    @Pattern(regexp = "^\\S+", message = "Nome de usuário não deve conter espaços.")
+    private String username;
+
+    @Length(min = 10, max = 100)
     private String password;
     private String description;
     private String curriculum;
